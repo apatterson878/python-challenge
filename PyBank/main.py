08e4c2ac473c806bf07dfbb6fe062380b4ca0d22
Ashley Patterson <ashleypatterson@AshPatt.local>
2019-02-04 17:00:18 -0600

    small changes working on %increase >increase

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ import csv
 theday = []
 price = []
 months = 0
+greatprice = 0
 
 csvpath = os.path.join( "Resources", "budget_data.csv")
 avgchange = 0.00
@@ -17,20 +18,26 @@ with open(csvpath, newline='') as csvfile:
 
 
     # # Read the header row first (skip this step if there is no header)
-    # csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
-
-    # # Read each row of data after the header
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    #print(csvreader)
+    # greatprice = int(csvreader[1])
+    # badprice = int(csvreader[1])
     
+    # # Read each row of data after the header  
     for row in csvreader:
         
         count+=1
         total+= int(row[1])
-        # print(int(row[1]))
         
         price.append(row[1])
         theday.append(row[0])
     
+        # if greatprice > int(next(row[1])):
+        #     greatprice = int(next(row))
+    print(int((row[1])))
+    print(str(price))
+
     lastprice = int(price[len(price)-1])
     firstprice = int(price[0])
     avgchange = (lastprice - firstprice)/85
