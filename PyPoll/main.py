08c599b73e1941e188f5e0539cab1172baa7fe72
Ashley Patterson <ashleypatterson@AshPatt.local>
2019-02-05 13:27:24 -0600

    work in progress

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,16 +6,26 @@ csvpath = os.path.join( "Resources", "election_data.csv")
 
 
 with open(csvpath, newline='') as csvfile:
+    count = 0
+    # next(csvfile)
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+       count+=1
+       
+print(count)
+
+
+print("")
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {count}")
+print("----------------------------")
