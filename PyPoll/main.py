aa2952d788a385d3007b9f1af451dad83ca7df82
Ashley Patterson <ashleypatterson@AshPatt.local>
2019-02-05 14:30:58 -0600

    small update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,10 @@
 import os
 import csv
 
+county = []
+candidate = []
+uniquetesting = ["john", "joe", "john", "mel"]
+unique_candidate = []
 
 csvpath = os.path.join( "Resources", "election_data.csv")
 
@@ -20,12 +24,20 @@ with open(csvpath, newline='') as csvfile:
     # Read each row of data after the header
     for row in csvreader:
        count+=1
-       
-print(count)
-
+       county.append(row[1])
+       candidate.append(row[2])
+    
+unique_candidate = list(set(candidate))
 
 print("")
 print("Election Results")
 print("----------------------------")
 print(f"Total Votes: {count}")
+print("----------------------------")
+print(f"{unique_candidate[0]}: ")
+print(f"{unique_candidate[1]}: ")
+print(f"{unique_candidate[2]}: ")
+print(f"{unique_candidate[3]}: ")
+print("----------------------------")
+print(f"Winner: ")
 print("----------------------------")
